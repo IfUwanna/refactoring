5feb6abbb9f83abe5a79d64d965e2e750adca421
Jihun Park <jihun.park@navercorp.com>
2022-08-04 10:08:39 +0900

    220804-1.4 statement() 쪼개기

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,32 +1,9 @@
-const playsJSON = {
-    hamlet: { name: "Hamlet", type: "tragedy" },
-    "as-like": { name: "As You Like It", type: "comedy" },
-    othello: { name: "Othello", type: "tragedy" },
-};
 
-const invoicesJSON = [
-    {
-        customer: "BigCo",
-        performances: [
-            {
-                playID: "hamlet",
-                audience: 55,
-            },
-            {
-                playID: "as-like",
-                audience: 35,
-            },
-            {
-                playID: "othello",
-                audience: 40,
-            },
-        ],
-    },
-];
+/**
+ * 1-1 Let's start
+ * */
+export function statement1_1(invoice, plays) {
 
-// 1-1. Let's start
-//console.log(statement(invoicesJSON[0],playsJSON));
-export function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -37,18 +14,19 @@ export function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
+
         const play = plays[perf.playID];
         let thisAmount = 0;
 
         switch (play.type) {
             case 'tragedy': // 비극
-                thisAmount = 40000;
+                thisAmount = 40000;  // 기본 4만원 + 30명 초과시 초과인원*1000원
                 if (perf.audience > 30) {
                     thisAmount += 1000 * (perf.audience - 30);
                 }
                 break;
             case 'comedy': // 희극
-                thisAmount = 30000;
+                thisAmount = 30000; // 기본 3만원 + 20명 초과시 1만원+(초과인원*1000원)
                 if (perf.audience > 20) {
                     thisAmount += 10000 + 500 * (perf.audience - 20);
                 }
@@ -57,19 +35,131 @@ export function statement(invoice, plays) {
             default:
                 throw new Error(`알 수 없는 장르: ${play.type}`);
         }
+        // 포인트 적립
+        volumeCredits += Math.max(perf.audience - 30, 0); // 30명 초과 인원당 1점
+        if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5); // comedy 관객 5명당 1점
 
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
-
-        // 청구 내역을 출력한다.
+        // 연극별 청구내역
         result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+
 }
 
+
+/**
+ * 1-4 statement 함수 쪼개기
+ * */
+export function statement1_4(invoice, plays) {
+
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+    for (let perf of invoice.performances) {
+        // 연극별 청구내역
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    }
+    result += `총액: ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    return result;
+
+
+//---------------------------------------------------------------------------
+    // 1. 공연비 계산 - 함수 추출
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case 'tragedy': // 비극
+                result = 40000;  // 기본 4만원 + 30명 초과시 초과인원*1000원
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case 'comedy': // 희극
+                result = 30000; // 기본 3만원 + 20명 초과시 1만원+(초과인원*1000원)
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
+
+    // 2. play 추출 - 임시 변수를 질의함수로 바꾸기
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    // 3. 포인트 계산 함수 추출
+    function volumeCreditsFor(perf) {
+        let volumeCredits = 0;
+        volumeCredits += Math.max(perf.audience - 30, 0); // 30명 초과 인원당 1점
+        if (playFor(perf).type === 'comedy') {                  // comedy 관객 5명당 1점
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
+        return volumeCredits;
+    }
+    // 4. format 변수 제거  - 함수 선언 바꾸기 적용 format > usd
+    function usd(aNumber) {
+        return new  Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2,
+        }).format(aNumber/100);
+    }
+    // 5. volumeCredits 변수제거 - 반복문 분리 > 문장슬라이드 > 함수 추출 > 변수 인라인
+    function totalVolumeCredits() {
+        let volumeCredits = 0;  //  volumeCredits 변수제거 = 문장 슬라이드하기
+        for (let perf of invoice.performances) { // volumeCredits 변수 제거 - 반복문 쪼개기,
+            volumeCredits += volumeCreditsFor(perf); // 포인트 적립 함수 추출
+        }
+        return volumeCredits;
+    }
+    // 6. totalAmount 변수 제거 - 반복문분리 > 문장슬라이드 > 함수추출 > 변수인라인
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
+
+}
+
+
+
+export function plays() {
+    return {
+        hamlet: {name: "Hamlet", type: "tragedy"},
+        "as-like": {name: "As You Like It", type: "comedy"},
+        othello: {name: "Othello", type: "tragedy"},
+    };
+}
+
+
+export function invoices() {
+    return [
+        {
+            customer: "BigCo",
+            performances: [
+                {
+                    playID: "hamlet",
+                    audience: 55,
+                },
+                {
+                    playID: "as-like",
+                    audience: 35,
+                },
+                {
+                    playID: "othello",
+                    audience: 40,
+                },
+            ],
+        },
+    ];
+}
--- chapter1/statement.test.js
+++ chapter1/statement.test.js
@@ -1,41 +1,20 @@
-import { statement } from "./statement.js";
+import * as s from "./statement.js";
 
-describe("statement", () => {
-    const playsJSON = {
-        hamlet: { name: "Hamlet", type: "tragedy" },
-        "as-like": { name: "As You Like It", type: "comedy" },
-        othello: { name: "Othello", type: "tragedy" },
-    };
+describe("chapter1. statement", () => {
 
-    const invoicesJSON = [
-        {
-            customer: "BigCo",
-            performances: [
-                {
-                    playID: "hamlet",
-                    audience: 55,
-                },
-                {
-                    playID: "as-like",
-                    audience: 35,
-                },
-                {
-                    playID: "othello",
-                    audience: 40,
-                },
-            ],
-        },
-    ];
-    it("should print a statement", () => {
-        const expected =
-            "청구 내역 (고객명: BigCo)\n" +
-            "  Hamlet: $650.00 (55석)\n" +
-            "  As You Like It: $580.00 (35석)\n" +
-            "  Othello: $500.00 (40석)\n" +
-            "총액: $1,730.00\n" +
-            "적립 포인트: 47점\n";
+    const expected =
+        "청구 내역 (고객명: BigCo)\n" +
+        "  Hamlet: $650.00 (55석)\n" +
+        "  As You Like It: $580.00 (35석)\n" +
+        "  Othello: $500.00 (40석)\n" +
+        "총액: $1,730.00\n" +
+        "적립 포인트: 47점\n";
 
-        expect(statement(invoicesJSON[0], playsJSON)).toBe(expected);
+    it("1.1 Let's start", () => {
+        expect(s.statement1_1(s.invoices()[0], s.plays())).toBe(expected);
+    });
+    it("1.4. statement() 함수 쪼개기", () => {
+        expect(s.statement1_4(s.invoices()[0], s.plays())).toBe(expected);
     });
 
 });
