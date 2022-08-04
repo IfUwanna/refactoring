b6691f0bec7055d3f1ed5a0ed83c629031a6caf2
Jihun Park <jihun.park@navercorp.com>
2022-08-04 13:33:36 +0900

    220804-1.6 계산단계와 포맷팅 단계 분리하기 - 파일분리전

--- chapter1/statement.test.js
+++ chapter1/statement.test.js
@@ -11,10 +11,13 @@ describe("chapter1. statement", () => {
         "적립 포인트: 47점\n";
 
     it("1.1 Let's start", () => {
-        expect(s.statement1_1(s.invoices()[0], s.plays())).toBe(expected);
+        expect(s.statement_1_1(s.invoices()[0], s.plays())).toBe(expected);
     });
     it("1.4. statement() 함수 쪼개기", () => {
-        expect(s.statement1_4(s.invoices()[0], s.plays())).toBe(expected);
+        expect(s.statement_1_4(s.invoices()[0], s.plays())).toBe(expected);
+    });
+    it("1.6. 계산 단계와 포맷팅 단계 분리하기", () => {
+        expect(s.statement_1_6(s.invoices()[0], s.plays())).toBe(expected);
     });
 
 });
