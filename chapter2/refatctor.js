97f9aa5791f9098191b5cf861cb3bfea9cac93f4
Jihun Park <jihun.park@navercorp.com>
2022-08-08 08:49:41 +0900

    220808 - 리팩터링 원칙

---
+++ chapter2/refatctor.js
@@ -0,0 +1,15 @@
+
+calculator([1, 2, 3, 4, 5]);
+function calculator(arr) {
+
+    let result = 0;
+    // 1. sum
+    result = arr.reduce((sum, p) => sum + p, 0);
+    console.log(`1. sum : ${result}`);
+
+    // 2. divide
+    result /= 2;
+    console.log(`2. divide : ${result}`);
+
+    return result;
+}
